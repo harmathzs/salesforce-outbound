3296e3b68fb16d89d62e846d90db09841376240e
Zsolt Harmath <harmathzs@gmail.com>
2025-11-09 11:38:10 +0100

    Implement getRawBody

--- react-app/api/order.js
+++ react-app/api/order.js
@@ -1,7 +1,15 @@
 /* order.js Vercel serverless function */
 import { parseString } from 'xml2js';
 
-export default function handler(req, res) {
+function getRawBody(req) {
+  return new Promise(resolve => {
+    let data = '';
+    req.on('data', chunk => data += chunk);
+    req.on('end', () => resolve(data));
+  });
+};
+
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).send('Method Not Allowed');
     return;
@@ -11,8 +19,9 @@ export default function handler(req, res) {
   const body = req.body
   console.log('request body', body)
 
-  let xml = '';
-  req.on('data', chunk => { xml += chunk; });
+  const xml = await getRawBody(req);
+  console.log('request body xml: ', xml);
+
   req.on('end', () => {
     parseString(xml, { explicitArray: false }, (err, result) => {
       if (err) {
